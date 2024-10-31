2fc3e316c61215d813639b2ad5fc35e9626de57e
@sachin.kaushik1 <sachin.kaushik@prismberry.com>
2024-10-31 16:59:01 +0530

    Node Server created

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+
+app.get("/", (req, res) =>{
+    res.json[
+        {
+            id: 1,
+            name: 'Sachin Kaushik',
+            age: 24
+        },
+        {
+            id: 2,
+            name: 'Naman Ojha',
+            age: 29
+        },
+        {
+            id: 3,
+            name: 'Aman Kumar',
+            age: 44
+        },
+        {
+            id: 4,
+            name: 'Vidya Sharma',
+            age: 19
+        }
+    ]
+});
+
+app.listen(5000, () => {
+    console.log("Server is running on 5000 port")
+})
